5479fd0c367473296bc5f83a5e1647310cca8af1
Rowen <wucenglou@qq.com>
2024-09-14 19:58:20 +0800

    1

--- controller/blog_controller.go
+++ controller/blog_controller.go
@@ -7,11 +7,12 @@ import (
 	"go-tiny/initialize"
 	"go-tiny/model"
 	"go-tiny/utils"
-	"log"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/olivere/elastic/v7"
+	"gorm.io/gorm"
 	"k8s.io/apimachinery/pkg/util/rand"
 )
 
@@ -65,8 +66,30 @@ func (bc *BlogController) CreateBlog(c *gin.Context) {
 		return
 	}
 	blog.AuthorID = user.ID
-
 	initialize.DB.Create(&blog)
+
+	// 将博客条目索引到 Elasticsearch
+	esBlog := map[string]interface{}{
+		"id":         blog.ID,
+		"title":      blog.Title,
+		"content":    blog.Content,
+		"author_id":  blog.AuthorID,
+		"created_at": blog.CreatedAt,
+		"updated_at": blog.UpdatedAt,
+	}
+
+	// 索引文档到 Elasticsearch
+	_, err := initialize.EsClient.Index().
+		Index("blogs").
+		Id(fmt.Sprintf("%d", blog.ID)).
+		BodyJson(esBlog).
+		Refresh("true").
+		Do(context.Background())
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to index blog to Elasticsearch"})
+		return
+	}
+
 	c.JSON(201, blog)
 }
 
@@ -130,41 +153,129 @@ func (bc *BlogController) SearchBlogs(c *gin.Context) {
 func (bc *BlogController) SearchBlogsByElastic(c *gin.Context) {
 	// 获取查询关键词
 	query := c.Query("keyword")
+	fmt.Println("Searching for:", query)
+	exists, err := initialize.EsClient.IndexExists("blogs").Do(context.Background())
+	if err != nil {
+		fmt.Println("Error checking index existence:", err)
+	}
+	if !exists {
+		fmt.Println("Index does not exist")
+	}
 
-	// 构建 Elasticsearch 查询语句
-	queryJSON := fmt.Sprintf(`
-	{
-		"query": {
-			"multi_match": {
-				"query": "%s",
-				"fields": ["title^2", "content"],
-				"fuzziness": "AUTO"
-			}
-		}
-	}`, query)
+	// 构建 Elasticsearch 查询
+	searchSource := elastic.NewSearchSource()
+	searchSource.Query(elastic.NewMultiMatchQuery(query, "title", "content").Type("best_fields").Operator("or").Analyzer("ik_smart"))
 
-	// 执行 Elasticsearch 查询
 	searchResult, err := initialize.EsClient.Search().
 		Index("blogs").
-		BodyString(queryJSON).
+		SearchSource(searchSource).
 		Do(context.Background())
 	if err != nil {
-		c.JSON(500, gin.H{"error": "Failed to search blogs"})
-		return
+		fmt.Println("Error searching:", err)
 	}
-
-	// 解析查询结果
-	blogs := make([]model.Blog, 0, len(searchResult.Hits.Hits))
-	for _, hit := range searchResult.Hits.Hits {
-		var blog model.Blog
-		err := json.Unmarshal(hit.Source, &blog)
-		if err != nil {
-			log.Printf("Error unmarshaling hit source: %v", err)
-			continue
-		}
-		blogs = append(blogs, blog)
-	}
-
 	// 返回查询结果
-	c.JSON(200, blogs)
+	c.JSON(200, searchResult)
+}
+
+// 处理数据索引
+func ExportBlogs(db *gorm.DB) ([]model.Blog, error) {
+	var blogs []model.Blog
+	err := db.Find(&blogs).Error
+	if err != nil {
+		return nil, err
+	}
+	return blogs, nil
+}
+
+func IndexBlogsToElasticsearch(esClient *elastic.Client, blogs []model.Blog) error {
+	for _, blog := range blogs {
+		// 查询用户信息以获取作者名称
+		var user model.User
+
+		key := fmt.Sprintf("%d", blog.AuthorID)
+
+		res := initialize.RedisClient.Get(context.Background(), key)
+		if res.Err() == nil {
+			userJSON := res.Val()
+			err := json.Unmarshal([]byte(userJSON), &user)
+			if err != nil {
+				fmt.Println("Error unmarshaling JSON:", err)
+			}
+		} else {
+			// 从数据库中查询用户信息
+			if err := initialize.DB.Where("id = ?", blog.AuthorID).First(&user).Error; err != nil {
+				return fmt.Errorf("failed to find user with ID %d: %w", blog.AuthorID, err)
+			}
+			userJson, _ := json.Marshal(user)
+			// 将用户信息缓存到 Redis
+			initialize.RedisClient.Set(context.Background(), key, userJson, 0)
+		}
+
+		// if err := initialize.DB.Where("id = ?", blog.AuthorID).First(&user).Error; err != nil {
+		// 	return fmt.Errorf("failed to find user with ID %d: %w", blog.AuthorID, err)
+		// }
+
+		// 构建 Elasticsearch 文档
+		esBlog := map[string]interface{}{
+			"id":        blog.ID,
+			"title":     blog.Title,
+			"content":   blog.Content,
+			"author_id": blog.AuthorID,
+			"author": map[string]interface{}{
+				"id":   user.ID,
+				"name": user.Username, // 假设 User 有一个 Name 字段
+			},
+			"published_at": blog.PublishedAt,
+			"created_at":   blog.CreatedAt,
+			"updated_at":   blog.UpdatedAt,
+		}
+
+		// 索引文档到 Elasticsearch
+		_, err := esClient.Index().
+			Index("blogs").
+			Id(fmt.Sprintf("%d", blog.ID)).
+			BodyJson(esBlog).
+			Refresh("wait_for").
+			Do(context.Background())
+		if err != nil {
+			return fmt.Errorf("failed to index blog to Elasticsearch: %w", err)
+		}
+	}
+	return nil
+}
+
+func IndexBlogToElasticsearch(esClient *elastic.Client, blog model.Blog) error {
+	// 查询用户信息以获取作者名称
+	var user model.User
+	if err := initialize.DB.Where("id = ?", blog.AuthorID).First(&user).Error; err != nil {
+		return err
+	}
+
+	// 构建 Elasticsearch 文档
+	esBlog := map[string]interface{}{
+		"id":        blog.ID,
+		"title":     blog.Title,
+		"content":   blog.Content,
+		"author_id": blog.AuthorID,
+		"author": map[string]interface{}{
+			"id":   user.ID,
+			"name": user.Username, // 假设 User 有一个 Name 字段
+		},
+		"published_at": blog.PublishedAt,
+		"created_at":   blog.CreatedAt,
+		"updated_at":   blog.UpdatedAt,
+	}
+
+	// 索引文档到 Elasticsearch
+	_, err := esClient.Index().
+		Index("blogs").
+		Id(fmt.Sprintf("%d", blog.ID)).
+		BodyJson(esBlog).
+		Refresh("wait_for").
+		Do(context.Background())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -12,13 +12,54 @@ import (
 func main() {
 	initialize.InitConfig()
 	initialize.InitRedis()
-	initialize.InitElastic()
+	// initialize.InitElastic()
 	// 初始化数据库连接
 	initialize.DB = initialize.InitDB()
 	if initialize.DB != nil {
 		db, _ := initialize.DB.DB()
 		defer db.Close()
 	}
+
+	// var wg sync.WaitGroup
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	// 导出博客条目
+	// 	blogs, err := controller.ExportBlogs(initialize.DB)
+	// 	if err != nil {
+	// 		log.Printf("Error exporting blogs: %s", err)
+	// 		return
+	// 	}
+
+	// 	// 使用 goroutines 并发处理每个博客条目，并限制最大并发数
+	// 	concurrentLimit := 200 // 最大并发数量
+	// 	semaphore := make(chan struct{}, concurrentLimit)
+	// 	blogWg := sync.WaitGroup{}
+
+	// 	blogWg.Add(len(blogs))
+
+	// 	// 使用 goroutines 并发处理每个博客条目
+	// 	for _, blog := range blogs {
+	// 		go func(blog model.Blog) {
+	// 			defer blogWg.Done()
+	// 			semaphore <- struct{}{}        // 获取信号量
+	// 			defer func() { <-semaphore }() // 释放信号量
+	// 			if err := controller.IndexBlogToElasticsearch(initialize.EsClient, blog); err != nil {
+	// 				log.Printf("Error indexing blog to Elasticsearch: %s", err)
+	// 			}
+	// 		}(blog)
+	// 	}
+
+	// 	// 等待所有 goroutines 完成
+	// 	blogWg.Wait()
+
+	// 	log.Println("All blogs indexed successfully.")
+	// }()
+
+	// go func() {
+	// 	wg.Wait()
+	// 	log.Println("Data import completed.")
+	// }()
 
 	initialize.RedisClient.Set(context.Background(), "test", "Hello, Redis!", 0)
 	r := gin.Default()
